92115b1b2b23c6e53c7804ba9c2c70ffaf393d5d
Halo104 <christopheryim@hotmail.com>
2021-11-17 11:23:16 +0000

    Created First Object

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const interestRate = 0.3;
 //General variable
 let theName = "James"; // String Literal
 let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let lastName = null;
 
-console.log(age);
+//Creating first object
+let person = {
+  firstName: "Mosh",
+  age: 40,
+};
+
+console.log(person);
