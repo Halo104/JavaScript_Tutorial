c5752b39a3fdc3d6135b02daf33ab422ba4e27b9
Halo104 <christopheryim@hotmail.com>
2021-11-17 11:39:04 +0000

    added arrays and functions

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-//How to create a constant in JS
+// How to create a constant in JS
 const interestRate = 0.3;
 
-//General variable
+// General variable
 let theName = "James"; // String Literal
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
 let firstName = undefined;
 let lastName = null;
 
-//Creating first object
+// Creating first object
 let person = {
   firstName: "Mosh",
   age: 40,
 };
 
-console.log(person);
+// Dot notation
+person.firstName = "John";
+
+// Bracket Notation, benifits is, it allows for variables as inputs, so it can be decided at run time
+let selection = "firstName";
+person[selection] = "Mary";
+
+// Arrays
+let selectedColours = ["red", "blue"];
+selectedColours[2] = 35;
+
+// Functions
+function greet(inputName) {
+  console.log("Hello " + inputName);
+}
+
+function square(inputNumber) {
+  output = inputNumber * inputNumber;
+  return output;
+}
+
+console.log(square(3));
